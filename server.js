2f1192379f42858eec613979d45b619d2d503e72
zky105043058 <zky105043058@163.com>
2016-07-03 00:17:37 +0800

    webgl-rotateCubic-

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+const dir = __dirname;
+const server = http.createServer(function(req,res){
+	var text;
+	if(req.url.match(/images|js/)){
+		text = fs.readFileSync(dir+'/webgl'+req.url);
+	}else{
+		text = fs.readFileSync(dir+'/webgl/1_rotateOnMouse.html');
+	}
+	res.end(text);
+});
+server.listen(80);
