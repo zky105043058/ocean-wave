f12899f632fd1b7f320f68268b0b38455d2aeef5
zky105043058 <zky105043058@163.com>
2016-07-03 22:10:55 +0800

    webgl3:click then change color

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = http.createServer(function(req,res){
 	var text;
 	if(req.url.match(/images|js/)){
 		text = fs.readFileSync(dir+'/webgl'+req.url);
+	}else if(req.url.indexOf('.html')>0){
+		console.log(dir+'/webgl'+req.url);
+		text = fs.readFileSync(dir+'/webgl'+req.url);
 	}else{
 		text = fs.readFileSync(dir+'/webgl/1_rotateOnMouse.html');
 	}
